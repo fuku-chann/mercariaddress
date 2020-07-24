dcbf96a1d78162d2d7f19b7d69294a6f5fdc403a
fuku-chann <64589657+fuku-chann@users.noreply.github.com>
2020-07-24 10:54:49 +0900

    get address only ship status

--- code.py
+++ code.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver import ActionChains
-#from selenium.webdriver.common.keys import keys
+#from selenium.webdriver.chrome.option import Options
 from time import sleep
 import chromedriver_binary
 import webbrowser
@@ -11,22 +11,44 @@ options = webdriver.ChromeOptions()
 options.add_argument(
    '--user-data-dir={chrom_dir_path}'.format(chrom_dir_path = '/Users/masa/Library/Application Support/Google/Chrome/Profile 3'))
 driver = webdriver.Chrome(options=options)
-sleep(5)
+sleep(3)
 
 # メルカリのページにアクセス
 driver.get("https://www.mercari.com/jp/mypage/listings/in_progress/")
 
 try:
-   # 要素のクラス名を指定して要素を取得
+   # 要素のクラス名を指定して取引ページの全てのURLを取得
+   elms = driver.find_elements_by_class_name("mypage-item-link")
+   status = driver.find_elements_by_class_name("mypage-item-body")
+   i = 0
+   lists = []
+   for elm in elms:
+      sta = status[i].text
+      #print(sta)
+      if '発送待ち' in sta:
+         elm = elms[i].get_attribute("href")
+         lists.append(elm)
+      i += 1
+   #取得したリンクの住所を取得
+   i = 0
+   for list in lists:
+      driver.get(lists[i])
+      #クラス名を指定して要素を特定
+      profit = driver.find_elements_by_css_selector("ul.transact-info-table-cell")
+      print(profit[6].text)
+      i += 1
+   
+   """
    item_page = driver.find_element_by_class_name("mypage-item-link")
    print(item_page.text)
    # 要素をクリック
    item_page.click()
-   sleep(5)
+   sleep(3)
    # クラス名を指定して要素を特定(CSSセレクタバージョン)
    profit = driver.find_elements_by_css_selector("ul.transact-info-table-cell")
    print(profit[3].text)
    print(profit[6].text)
+   """
 except Exception as e:
    print('No item')
    print(e)
@@ -37,34 +59,30 @@ options = webdriver.ChromeOptions()
 options.add_argument(
    '--user-data-dir={chrom_dir_path}'.format(chrom_dir_path = '/Users/masa/Library/Application Support/Google/Chrome/Profile 2'))
 driver = webdriver.Chrome(options=options)
-sleep(5)
+sleep(3)
 
 # メルカリのページにアクセス
-driver.get("https://www.mercari.com/jp/mypage/listings/in_progress/")
+driver.get('https://www.mercari.com/jp/mypage/listings/in_progress/')
 try:
-   # 要素のクラス名を指定して要素を取得
-   #item_page = driver.find_elements_by_class_name("mypage-item-link")
-   #item_page = driver.find_element_by_class_name("mypage-item-link").find_element_by_tag_name("a").get_attribute("href")
-   listOfLi = driver.find_elements_by_class_name("mypage-item-link")
-   for eachLiElement in listOfLi:
-      item_page = eachLiElement.get_attribute("href")   
-      #print(driver.find_elements_by_class_name("mypage-item-link"))
-      #item_page = driver.find_element_by_class_name("mypage-item-link")
-      print(item_page)
-      sleep(5)
-      url = item_page
-      webbrowser.open_new(url)
-      # 要素をクリック
-      #string.click()
-      #ActionChains(driver).key_down(Keys.CONTROL).click(string).key_up(Keys.CONTROL).perform()
-      # クラス名を指定して要素を特定(CSSセレクタバージョン)
+   # 要素のクラス名を指定して取引ページの全てのURLを取得
+   elms = driver.find_elements_by_class_name("mypage-item-link")
+   status = driver.find_elements_by_class_name("mypage-item-body")
+   i = 0
+   lists = []
+   for elm in elms:
+      sta = status[i].text
+      #print(sta)
+      if '発送待ち' in sta:
+         elm = elms[i].get_attribute("href")
+         lists.append(elm)
+      i += 1
+   #取得したリンクの住所を取得
+   i = 0
+   for list in lists:
+      driver.get(lists[i])
+      #クラス名を指定して要素を特定
       profit = driver.find_elements_by_css_selector("ul.transact-info-table-cell")
-      print(profit[1].text)
-      #print(profit[6].text)
-      # メルカリのページにアクセス
-      driver.back()
-      #driver.get("https://www.mercari.com/jp/mypage/listings/in_progress/")
-      sleep(1)
+      print(profit[6].text)
+      i += 1
 except Exception as e:
-   print('No item')
    print(e)
